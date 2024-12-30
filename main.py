afe0391e8d1cec10d1f0c9ba9ff20452aafdb204
Raikuha <65867276+Raikuha@users.noreply.github.com>
2024-12-30 09:15:03 -0300

    Added basic player movement

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import pygame
-import constants as c
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
     print("Starting asteroids!")
 
-    screen = pygame.display.set_mode((c.SCREEN_WIDTH, c.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
@@ -13,7 +20,10 @@ def main():
                 return
 
         screen.fill(color='black')
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 # This line ensures main() only runs if this file is executed directly
 # so it would never run if it were to be imported from other files
